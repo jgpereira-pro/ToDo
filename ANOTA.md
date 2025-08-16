9ba9c09ef0dc5a9afe6405c0b9899353cbffec40
cavera <jgpereirafreitas@gmail.com>
2025-08-15 21:48:24 -0300

    segunda aula

--- ANOTA.md
+++ ANOTA.md
@@ -7,3 +7,12 @@
 6. git push
 
 # documentacao do ToDo
+createElemente() -> Cria um novo elemento
+appenChild() -> Insere uma tag filho na tag pai
+innerHTML -> Insere um elemento dentro de outro elemento na mesma hierarquia
+classList -> Atributo que acessa a lista de classes na tag
+add() -> Adiciona alguma coisa
+
+preciso pegar a constante conteudo inserir ela dentro de uma tag li e inserir essa tag na ul
+
+Preciso incluir um botao no item toda vez que eu criar um novo item
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const lista = document.querySelector("{data-list}");
-
-const novaTarefa = document.querySelector("{data-form-button}");
-
 const criarTarefa = (evento) => {
     evento.preventDefault();
-    const input = document.querySelector("{data.fore-input}");
-    const valor = input.valeu;
-    console.log(valor);
-}
+    
+    const lista = document.querySelector("[data-list]");
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+    
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+    
+    input.value = "";
+};
 
-novaTarefa.addEventListener('click', criarTarefa);
+const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa); 
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = "concluir";
+    
+    botaoConclui.addEventListener('click', () => {
+        console.log("Fui clicado");
+    });
+    return botaoConclui;
+};
